a4f850705a1db8b0cc74a6291a924ececd68d079
Hyeyoung Kim <sunzero0116@gmail.com>
2021-06-24 01:25:01 +0900

    Update 핸드폰 번호 가리기.py

--- Implementation/핸드폰 번호 가리기.py
+++ Implementation/핸드폰 번호 가리기.py
@@ -1,6 +1,7 @@
 '''
-1. len 받기
-2. length-4 길이만큼 * 표시 + phone_number 뒤에서 4자리까지 표시
+코드 설명
+1) len 받기
+2) length-4 길이만큼 * 표시 + phone_number 뒤에서 4자리까지 표시
 '''
 def solution(phone_number):
     length = len(phone_number)
