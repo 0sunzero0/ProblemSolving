bbf88adee9c4d36f7b4a0b4a3ba42f61840cd205
Hyeyoung Kim <sunzero0116@gmail.com>
2021-08-29 23:21:56 +0900

    Update palindrom2.py

--- LinkedList/palindrom2.py
+++ LinkedList/palindrom2.py
@@ -20,3 +20,11 @@ class Solution:
         while rev and rev.val == slow.val:
             slow, rev = slow.next, rev.next
         return not rev
+
+   '''
+    1. runner 기법
+        연결리스트를 순회할 때 2개의 포인터를 동시 사용하는 기법
+        빠른 런너가 연결리스트의 끝에 도달하면 느린 런너는 정확히 연결리스트가 중간 지점을 가리키게 된다.
+        이 같은 방식으로 중간 위치르 찾아내면, 여기서부터 값을 비교하거나 뒤집기를 시도하는 드 여러모로 활용할 수 있다. 
+    2. 다중 할당
+   '''
