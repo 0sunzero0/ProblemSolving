07da7b7ac90935eba252089bf5829d196f511021
Hyeyoung Kim <sunzero0116@gmail.com>
2022-02-05 19:33:32 +0900

    refactor : 1로 만들기
    - input 값 범위 고려하여, base 재설정

--- DP/Baek_1463.py
+++ DP/Baek_1463.py
@@ -1,7 +1,5 @@
 def make_one(N):
-    dp[1] = 0
-    dp[2] = 1
-    for i in range(3, N+1):
+    for i in range(2, N+1):
         dp[i] = dp[i-1] + 1
         if i % 2 == 0:
             dp[i] = min(dp[i], dp[i//2] + 1)
