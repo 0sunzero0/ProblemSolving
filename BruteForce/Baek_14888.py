68139957a8c5572f194f27a9bf1a74518bd7f274
Hyeyoung Kim <sunzero0116@gmail.com>
2022-03-04 20:05:29 +0900

    refactor : 연산자 끼워넣기 (재귀)

--- BruteForce/Baek_14888.py
+++ BruteForce/Baek_14888.py
@@ -1,48 +1,37 @@
-from itertools import permutations
-
 N = int(input())
 operands = list(map(int, input().split()))
 operators_count = list(map(int, input().split()))
 operator_type = ['+', '-', '*', '/']
-operators = []
 max_value = -1000000000
 min_value = 1000000000
 
-for i in range(4):
-    for _ in range(operators_count[i]):
-        operators.append(operator_type[i])
-
-def calculate(operator):
-    result = operands[0]
-    for i in range(0, len(operator)):
-        if operator[i] == '+':
-            result += operands[i+1]
-        elif operator[i] == '-':
-            result -= operands[i+1]
-        elif operator[i] == '*':
-            result *= operands[i+1]
-        elif operator[i] == '/':
-            if result < 0 and operands[i+1] > 0:
-                result = -((-result) // operands[i + 1])
-            else:
-                result //= operands[i+1]
+def calculator(operand1, candidate, operand2):
+    if operator_type[candidate] == '+':
+        result = operand1 + operand2
+    elif operator_type[candidate] == '-':
+        result = operand1 - operand2
+    elif operator_type[candidate] == '*':
+        result = operand1 * operand2
+    elif operator_type[candidate] == '/':
+        if operand1 < 0 and operand2 > 0:
+            result = -((-operand1) // operand2)
+        else:
+            result = operand1 // operand2
     return result
 
-for operator in list(permutations(operators, len(operators))):
-    value = calculate(operator)
-    max_value = max(value, max_value)
-    min_value = min(value, min_value)
+def calculate(index, result):
+    global min_value, max_value
+    if index == N-1:
+        min_value = min(min_value, result)
+        max_value = max(max_value, result)
+    else:
+        global operators_count
+        for candidate in range(4):
+            if operators_count[candidate] >= 1:
+                operators_count[candidate] -= 1
+                calculate(index + 1, calculator(result, candidate, operands[index + 1]))
+                operators_count[candidate] += 1
 
+calculate(0, operands[0])
 print(max_value)
 print(min_value)
-
-'''
-3
-3 4 5
-1 0 1 0
-'''
-'''
-7
-1 2 3 4 5 6 8
-2 2 1 1
-'''
