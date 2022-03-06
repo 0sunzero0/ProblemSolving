b7abeb13ba41abf0718229a59d2be81042e2e433
Hyeyoung Kim <sunzero0116@gmail.com>
2022-03-07 02:30:48 +0900

    refactor : 오픈채팅방(2019 KAKAO BLIND)

--- Kakao/2019_오픈채팅방.py
+++ Kakao/2019_오픈채팅방.py
@@ -1,18 +1,27 @@
 def solution(record):
-    result = []
     user_nickname = {}
+    events = []
+    message = []
 
     for action in record:
-        if action[0] == "E" or action[0] == "C":
-            command, id, nickname = action.split(" ")
+        action = action.split(" ")
+        command = action[0]
+        id = action[1]
+
+        if command == "Enter" or command == "Change":
+            nickname = action[2]
             user_nickname[id] = nickname
-
-    for action in record:
-        element = action.split(" ")
-        command, id = element[0], element[1]
         if command == "Enter":
-            result.append(user_nickname.get(id) + "님이 들어왔습니다.")
-        elif command == "Leave":
-            result.append(user_nickname.get(id) + "님이 나갔습니다.")
+            events.append(id + "님이 들어왔습니다.")
+        if command == "Leave":
+            events.append(id + "님이 나갔습니다.")
 
-    return result
+    for event in events:
+        event_split = event.split("님")
+        id = event_split[0]
+        action = event_split[1]
+        message.append(user_nickname[id] + "님" + action)
+
+    return message
+
+solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
