059e870d5ee52f283c95b1919a59e57913609226
Hyeyoung Kim <sunzero0116@gmail.com>
2022-03-10 16:43:14 +0900

    feat : 연결 요소의 개수

---
+++ Graph/Baek_11724.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+N, M = map(int, input().split())
+graph = {i : [] for i in range(1, N+1)}
+visited = [False] * (N+1)
+count = 0
+
+for _ in range(M):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
+
+def bfs(v):
+    queue = deque()
+    queue.append(v)
+
+    while queue:
+        vertex = queue.popleft()
+        for next_vertex in graph[vertex]:
+            if visited[next_vertex] == False:
+                visited[next_vertex] = True
+                queue.append(next_vertex)
+
+def dfs(v):
+    for next_vertex in graph[v]:
+        if visited[next_vertex] == False:
+            visited[next_vertex] = True
+            dfs(next_vertex)
+
+for v in range(1, N+1):
+    if visited[v] == False:
+        # bfs(v)
+        dfs(v)
+        count += 1
+
+print(count)
+
+'''
+6 5
+1 2
+2 5
+5 1
+3 4
+4 6
+'''
