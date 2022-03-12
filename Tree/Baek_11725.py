9b5e704ce083ff97ee68cd0f350ee7fdd2c5b4f5
Hyeyoung Kim <sunzero0116@gmail.com>
2022-03-12 20:26:22 +0900

    fix : 트리의 부모 찾기

--- Tree/Baek_11725.py
+++ Tree/Baek_11725.py
@@ -1,9 +1,12 @@
+import sys
+
 N = int(input())
-graph = [[] for _ in range(N + 1)]
-parent = [0] * (N + 1)
+graph = {vertex : [] for vertex in range(1, N + 1)}
+parents = [0] * (N + 1)
+sys.setrecursionlimit(100000)
 
 for _ in range(N - 1):
-    x, y = list(map(int, input().split()))
+    x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
@@ -11,10 +14,20 @@ def dfs(vertex, parent):
         for next_vertex in graph[vertex]:
             if next_vertex == parent:
                 continue
-            parent[next_vertex] = vertex
+            parents[next_vertex] = vertex
             dfs(next_vertex, vertex)
 
 dfs(1, -1)
 
 for vertex in range(2, N + 1):
-    print(parent[vertex])
+    print(parents[vertex])
+
+'''
+7
+1 6
+6 3
+3 5
+4 1
+2 4
+4 7
+'''
