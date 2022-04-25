f674da56bb7cb580dbad6effb039c0675fb57caa
Hyeyoung Kim <sunzero0116@gmail.com>
2022-04-25 21:10:22 +0900

    refactor : 평범한 배낭

--- DP/Baek_12865.py
+++ DP/Baek_12865.py
@@ -1,25 +1,19 @@
 N, K = map(int, input().split())
-
-thing = [[0, 0]]
-dp = [[0] * (K + 1) for _ in range(N + 1)]
-
+dp = [[0] * (K+1) for _ in range(N+1)]
+things = [[0, 0]]
 for _ in range(N):
-    thing.append(list(map(int, input().split())))
+    things.append(list(map(int, input().split())))
 
-for i in range(1, N + 1):
-    for j in range(1, K + 1):
-        weight = thing[i][0]
-        value = thing[i][1]
-
-        # 무게가 허용무게 보다 크면,
-        if weight > j:
-            # 이 물건을 담지 않고 이전 물건까지 담았을 때 가방에 담을 수 있는 최고 가치를 저장한다.
-            dp[i][j] = dp[i - 1][j]
-        else:
-            # 현재 물건을 넣지 않았을 때와 현재 물건을 넣었을 때의 가치를 비교한다.
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
+for i in range(1, N+1):
+    for w in range(1, K+1):
+        weight = things[i][0]
+        value = things[i][1]
+        dp[i][w] = dp[i - 1][w]
+        if w - weight >= 0:
+            dp[i][w] = max(dp[i-1][w], dp[i-1][w-weight] + value)
 
 print(dp[N][K])
+
 '''
 4 7
 6 13
