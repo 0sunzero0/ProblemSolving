c7bf7f7f9e1a73a71e8f3f53a1afb706dc321886
Hyeyoung Kim <sunzero0116@gmail.com>
2022-05-04 00:06:51 +0900

    feat : 1, 2, 3 더하기

---
+++ DP/Baek_9095.py
@@ -0,0 +1,16 @@
+def get_methods_num(N):
+    dp = [0] * (N + 1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    return dp[N]
+
+
+test_case = int(input())
+for _ in range(test_case):
+    N = int(input())
+    print(get_methods_num(N))
