561383a27539206dfcd4f40da565d05db2344419
Hyeyoung Kim <sunzero0116@gmail.com>
2022-05-04 17:32:22 +0900

    refactor : 1, 2, 3 더하기

--- DP/Baek_9095.py
+++ DP/Baek_9095.py
@@ -1,9 +1,12 @@
 def get_methods_num(N):
     dp = [0] * (N + 1)
     dp[0] = 0
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
+    if N >= 1:
+        dp[1] = 1
+    if N >= 2:
+        dp[2] = 2
+    if N >= 3:
+        dp[3] = 4
 
     for i in range(4, N + 1):
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
