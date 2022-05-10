7de0988851e92b92bc150f7548fafd1a1d131763
Hyeyoung Kim <sunzero0116@gmail.com>
2022-05-10 11:13:24 +0900

    feat : N과 M (9)

---
+++ BruteForce/Baek_15663.py
@@ -0,0 +1,39 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort()
+visited = [False] * N
+selected = []
+
+
+def go(depth):
+    if depth == M:
+        print(*selected)
+        return
+
+    overlap = 0
+    for i in range(N):
+        if not visited[i] and overlap != arr[i]:
+            visited[i] = True
+            selected.append(arr[i])
+            overlap = arr[i]
+
+            go(depth + 1)
+
+            visited[i] = False
+            selected.pop()
+
+
+go(0)
+'''
+3 1
+4 4 2
+'''
+'''
+4 2
+9 7 9 1
+'''
+'''
+4 4
+1 1 1 1
+'''
