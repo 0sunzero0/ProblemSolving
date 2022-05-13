b53cb9273fe0b882a5c43fde476bbada2c7fb69d
hyeyoungs <sunzero0116@gmail.com>
2022-05-13 17:21:22 +0900

    refactor : 수들의 합 2

--- TwoPointer/Baek_2003.py
+++ TwoPointer/Baek_2003.py
@@ -1,26 +1,26 @@
-length, sum = map(int, input().split())
+N, M = tuple(map(int, input().split()))
 num = list(map(int, input().split()))
-cumulated_sum = [num[0]] * (length)
+
+cumulated_sum = [num[0]] * N
 count = 0
 
-if length >= 2:
-    for i in range(1, length):
-        cumulated_sum[i] = cumulated_sum[i-1] + num[i]
+if N >= 2:
+    for i in range(1, N):
+        cumulated_sum[i] = cumulated_sum[i - 1] + num[i]
 
 left, right = 0, 0
-while True:
-    if left > right or right >= length :   break
+while left <= right < N:
 
     if left == 0:
         prefix_sum = cumulated_sum[right]
     else:
-        prefix_sum = cumulated_sum[right] - cumulated_sum[left-1]
+        prefix_sum = cumulated_sum[right] - cumulated_sum[left - 1]
 
-    if prefix_sum < sum:
+    if prefix_sum < M:
         right += 1
-    elif prefix_sum > sum:
+    elif prefix_sum > M:
         left += 1
-        if left > right and left < length:
+        if right < left < N:
             right = left
     else:
         count += 1
@@ -31,4 +31,8 @@ print(count)
 '''
 4 2
 1 1 1 1
+'''
+'''
+10 5
+1 2 3 4 2 5 3 1 1 2
 '''
