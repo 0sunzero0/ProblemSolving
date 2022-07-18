f78907c3cb2a9a68fcc1b2186d25f25f7884372d
hyeyoungs <sunzero0116@gmail.com>
2022-07-18 19:14:27 +0900

    refactor : 인구 이동

--- SW Capability Test/Baek_16234.py
+++ SW Capability Test/Baek_16234.py
@@ -1,53 +1,66 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-N, L, R = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+N, L, R = tuple(map(int, input().split()))
+grid = [list(map(int, input().split())) for _ in range(N)]
 
-def bfs():
-    is_united = False
-    visited = [[False] * N for _ in range(N)]
 
-    for i in range(0, N):
-        for j in range(0, N):
-            if visited[i][j] == False:
-                queue = deque()
-                queue.append((i, j))
-                visited[i][j] = True
-                united = deque()
-                united.append((i, j))
-                unit_population = graph[i][j]
+def in_range(y, x):
+   return 0 <= y < N and 0 <= x < N
 
-                while queue:
-                    y, x = queue.popleft()
 
-                    for index in range(4):
-                        ny, nx = y + dy[index], x + dx[index]
-                        if 0 <= ny < N and 0 <= nx < N and visited[ny][nx] == False:
-                            if L <= abs(graph[y][x] - graph[ny][nx]) <= R:
-                                queue.append((ny, nx))
-                                visited[ny][nx] = True
-                                united.append((ny, nx))
-                                unit_population += graph[ny][nx]
-                                is_united = True
+def can_go(ny, nx, y, x):
+    return L <= abs(grid[ny][nx] - grid[y][x]) <= R and not visit[ny][nx]
 
-                value = unit_population // len(united)
-                while united:
-                    y, x = united.popleft()
-                    graph[y][x] = value
 
-    return is_united
+def bfs_unit(sy, sx):
+    dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
+    bfs_queue = deque()
+    visit[sy][sx] = True
+    bfs_queue.append((sy, sx))
 
-day = 0
+    count = 1
+    populations = grid[sy][sx]
+    pos = [(sy, sx)]
+
+    while bfs_queue:
+        y, x = bfs_queue.popleft()
+        for i in range(4):
+            ny, nx = dy[i] + y, dx[i] + x
+            if in_range(ny, nx) and can_go(ny, nx, y, x):
+                count += 1
+                populations += grid[ny][nx]
+                pos.append((ny, nx))
+
+                visit[ny][nx] = True
+                bfs_queue.append((ny, nx))
+
+    return count, populations, pos
+
+
+def simulate():
+    global unit_count
+
+    for i in range(N):
+        for j in range(N):
+            if not visit[i][j]:
+                count, populations, pos = bfs_unit(i, j)
+            for y, x in pos:
+                grid[y][x] = populations // count
+            if count >= 2:
+                unit_count += 1
+
+
+time = 0
+
 while True:
-    if bfs() == False:
+    visit = [[False] * N for _ in range(N)]
+    unit_count = 0
+    simulate()
+    if unit_count == 0:
         break
-    day += 1
+    time += 1
 
-print(day)
+print(time)
 
 '''
 2 20 50
