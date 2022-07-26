9e35064a1145db93735f2c0b27fd6368c9f8a40d
hyeyoungs <sunzero0116@gmail.com>
2022-07-26 23:56:00 +0900

    refactor : 양과 늑대 (2022 KAKAO BLIND)

--- Kakao/2022B_양과 늑대.py
+++ Kakao/2022B_양과 늑대.py
@@ -1,26 +1,34 @@
-max_sheep = 0
+answer = 0
 
 def solution(info, edges):
-    tree = {vertex: [] for vertex in range(len(info))}
+    global answer
+    N = len(info)
+
+    # tree 만들기
+    tree = {vertex: [] for vertex in range(N)}
     for edge in edges:
-        start, end = edge[0], edge[1]
+        start, end = edge
         tree[start].append(end)
 
-    def dfs(vertex, sheep, wolf, next_vertexs):
-        global max_sheep
-        sheep += info[vertex] ^ 1
-        wolf += info[vertex]
+    # 백트레킹 함수
+    def go(current_vertex, sheep, wolf, next_vertexs):
+        global answer
+
+        sheep += info[current_vertex] ^ 1  # xor
+        wolf += info[current_vertex]
 
         if sheep <= wolf:
             return
 
-        max_sheep = max(max_sheep, sheep)
-        for next_vertex in tree[vertex]:
+        answer = max(answer, sheep)
+
+        for next_vertex in tree[current_vertex]:
             next_vertexs.add(next_vertex)
 
         for next_vertex in next_vertexs:
-            dfs(next_vertex, sheep, wolf, next_vertexs - set([next_vertex]))
+            go(next_vertex, sheep, wolf, next_vertexs - set([next_vertex]))
 
-    dfs(0, 0, 0, set())
+    # 백트레킹 함수 호출
+    go(0, 0, 0, set())
 
-    return max_sheep
+    return answer
